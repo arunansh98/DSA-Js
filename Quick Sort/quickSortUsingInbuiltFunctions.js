e4cd450dfafc8b62ffe9796c226eccedd7a51507
Arunansh Srivastava <arunansh98@gmail.com>
2023-07-29 15:17:02 +0530

    Create quickSort.js and quickSortUsingInbuiltFunctions.js

---
+++ Quick Sort/quickSortUsingInbuiltFunctions.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var sortArray = function(nums) {
+    return quickSort(nums);
+}
+
+var quickSort = function(arr) {
+    if(arr.length>0){ 
+        let pivot = arr[getRndInteger(0,arr.length-1)]; 
+        let smaller = arr.filter(ele=>ele<pivot);
+        let greater = arr.filter(ele=>ele>pivot);
+        return [...quickSort(smaller),...Array(arr.length-smaller.length-greater.length).fill(pivot),...quickSort(greater)];
+    }
+    else {
+        return [];
+    }
+}
+
+var getRndInteger= function(min, max) {
+  return Math.floor(Math.random() * (max - min) ) + min;
+}
+
+console.log(sortArray([5,4,3,-1,-1,2,1]))

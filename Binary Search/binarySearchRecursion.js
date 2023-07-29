89289b128fd31067d96399ddc81daad6c948b218
Arunansh Srivastava <arunansh98@gmail.com>
2023-07-29 20:43:41 +0530

    Create count.js,greater.js and binarySearchRecursion.js

---
+++ Binary Search/binarySearchRecursion.js
@@ -0,0 +1,24 @@
+/* 
+Write a program to search an element in a sorted array using binary search and recursion
+*/
+
+let arr = [-1,1,3,5,8] 
+
+let element = 7;
+
+var binarySearch = function(arr,ele,low,high){
+    mid = parseInt((low + high)/2);
+    if(arr[mid]===ele){
+        return mid;
+    }
+    else {
+        if(ele > arr[mid]){
+            return binarySearch(arr,ele,mid + 1,high);
+        }
+        else {
+            return binarySearch(arr,ele,low,mid-1);
+        }
+    }
+}
+
+console.log(binarySearch(arr,3,0,arr.length-1))
